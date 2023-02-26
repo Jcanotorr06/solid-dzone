a7fdc3871e1ca2457f16bd62ad33b06a3963bd8a
Jcanotorr06 <jcano062001@gmail.com>
2023-02-26 03:53:22 -0500

    update: remove unused types

--- src/types.ts
+++ src/types.ts
@@ -10,30 +10,6 @@ export type UploadFile = {
   file: File
 }
 
-export interface Dropzone<T extends HTMLElement = HTMLElement> {
-  setRef: (ref: T) => void
-  files: Accessor<UploadFile[]>
-  isDragging: Accessor<boolean>
-  removeFile: (fileName: string) => void
-  clearFiles: () => void
-}
-
-export type UserCallback = (files: UploadFile[]) => void | Promise<void>
-
-/**
- * @property `accept` - Comma-separated list of one or more file types, or unique file type specifiers
- * @link `accept` - https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/accept
- */
-export interface DropzoneOptions {
-  onDrop?: UserCallback
-  onDragStart?: UserCallback
-  onDragEnter?: UserCallback
-  onDragEnd?: UserCallback
-  onDragLeave?: UserCallback
-  onDragOver?: UserCallback
-  onDrag?: UserCallback
-}
-
 export type FileErrors = {
   [key: string]: {
     error: string
